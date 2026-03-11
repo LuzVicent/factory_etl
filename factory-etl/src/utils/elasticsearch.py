482d6585664989bb8872f32d7fac0f4170bcf6f0
Luz Vicent Gigante <luzvicgig@alu.edu.gva.es>
2026-03-11 19:43:51 +0100

    Add files via upload

---
+++ factory-etl/src/utils/elasticsearch.py
@@ -0,0 +1,22 @@
+from elasticsearch import Elasticsearch
+import os
+
+ELASTICSEARCH_HOST = os.getenv("ELASTICSEARCH_HOST")
+ELASTICSEARCH_PORT = os.getenv("ELASTICSEARCH_PORT")
+ELASTICSEARCH_RAW_INDEX = os.getenv("ELASTICSEARCH_RAW_INDEX")
+ELASTICSEARCH_RICH_INDEX = os.getenv("ELASTICSEARCH_RICH_INDEX")
+
+client = Elasticsearch(F'{ELASTICSEARCH_HOST}:{ELASTICSEARCH_PORT}',
+                       headers={
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    })
+
+def save_raw_event(event):
+    try:
+        client.index(index=ELASTICSEARCH_RAW_INDEX, document=event)
+    except Exception as e:
+        print(f"Error saving raw event: {e}")
+
+def save_rich_event(event):
+    client.index(index=ELASTICSEARCH_RICH_INDEX, document=event)
